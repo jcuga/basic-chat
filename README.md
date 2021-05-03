7fdeac60563952a5c552f12b2d2991876dd91612
Justin <justin@examle.com>
2021-05-03 08:09:49 -0400

    working on site skeleton

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ go build basic-chat.go
 
 ## To Run
 ```bash
-./basic-chat -serve 127.0.0.1:8080 -clientJs ./static/golongpoll-client.js -accounts "username1:password1,username2:password2,username3:password3"
+./basic-chat -serve 127.0.0.1:8080 -static ./static/ -accounts "username1:password1,username2:password2,username3:password3"
 ```
 
 Then visit `http://127.0.0.1:8080` and log in using HTTP basic auth with one of the accounts.
@@ -17,7 +17,7 @@ Then visit `http://127.0.0.1:8080` and log in using HTTP basic auth with one of 
 Configure via command-line arguments:
 
 * `-serve` specifies the address to serve HTTP.
-* `-clientJs` specifies where the `./static/golongpoll-client.js` file is located for serving via HTTP as a static file. This needs to be the location of the file *relative to where the executable is run from*.
+* `-static` specifies where the `./static/` directory is located for serving via HTTP. This must be *relative to where the executable is run from* or an absolute path..
 * `-saveChatsTo` is the filename to persist chats to. This way chat history is preserved across server restarts.
 * `-accounts` is a comma delimited list of `username:password` accounts. Only these users will be allowed to chat.
 * `-historySize` is the number of chats to retain per chat room, defaults to 250. This history is present entirely in-memory as well as persisted to disk.
--- basic-chat.go
+++ basic-chat.go
@@ -4,10 +4,10 @@ import (
 	"crypto/subtle"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/jcuga/golongpoll"
@@ -20,7 +20,7 @@ type User struct {
 
 func main() {
 	serveAddr := flag.String("serve", "127.0.0.1:8080", "Address to serve HTTP on.")
-	staticClientJs := flag.String("clientJs", "./static/golongpoll-client.js", "where the static/golongpoll-client.js is located relative to where this binary runs.")
+	staticDir := flag.String("static", "./static/", "where the static directory is located relative to where this binary runs.")
 	persistFilename := flag.String("saveChatsTo", "./chats.json", "Where to save chat history so not lost when server is restarted.")
 	usersList := flag.String("accounts", "", "comma delimited user:pwd for basic auth accounts.")
 	chatHistorySize := flag.Uint("-historySize", 250, "Max number of chats in history per chat room.")
@@ -38,20 +38,13 @@ func main() {
 		fmt.Printf("Must provide at least one account (user:password) via -accounts flag")
 		os.Exit(1)
 	}
-
 	if *chatHistorySize < 10 {
 		fmt.Printf("-historySize must be >= 10, got: %d", *chatHistorySize)
 		os.Exit(1)
 	}
 
-	// serve longpoll js client code
-	http.HandleFunc("/js/golongpoll-client.js", requireBasicAuth(
-		func(w http.ResponseWriter, r *http.Request) {
-			http.ServeFile(w, r, *staticClientJs)
-		}, users))
-
 	manager, err := golongpoll.StartLongpoll(golongpoll.Options{
-		// hang on to 2500 most recent chats:
+		// hang on to N most recent chats:
 		MaxEventBufferSize: int(*chatHistorySize),
 		AddOn:              filePersistor,
 	})
@@ -59,20 +52,24 @@ func main() {
 		log.Fatalf("Failed to create chat longpoll manager: %q\n", err)
 	}
 
+	// web app pages
 	http.HandleFunc("/", requireBasicAuth(indexPage, users))
 	http.HandleFunc("/chat", requireBasicAuth(chatroomPage, users))
-	http.HandleFunc("/send", requireBasicAuth(manager.PublishHandler, users))
-	http.HandleFunc("/listen", requireBasicAuth(manager.SubscriptionHandler, users))
-	http.HandleFunc("/logout", logoutPage)
+	http.HandleFunc("/logout", requireBasicAuth(logoutPage, users))
 
-	http.HandleFunc("/favicon.svg", requireBasicAuth(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "image/svg+xml")
-		fmt.Fprintf(w,
-			`<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" viewBox="0 0 100 100"><rect width="100" height="100" rx="20" fill="#7d6ee7"></rect><path fill="#fff" d="M30.10 38.10L30.10 38.10Q31.10 38.10 32.10 38.50Q33.10 38.90 34.30 39.30L34.30 39.30L44.90 44.70L44.30 25.90Q45.30 25.30 46.90 24.70Q48.50 24.10 50.50 24.10L50.50 24.10Q53.10 24.10 54.80 25.30Q56.50 26.50 56.50 29.30L56.50 29.30Q56.50 30.70 56.10 35.70Q55.70 40.70 55.50 45.10L55.50 45.10L70.90 36.30Q72.70 37.10 74.30 39Q75.90 40.90 75.90 43.30L75.90 43.30Q75.90 45.70 74.50 46.90Q73.10 48.10 70.90 49.10L70.90 49.10L59.10 54.70L71.70 69.70Q71.30 72.30 69.20 74.10Q67.10 75.90 64.70 75.90L64.70 75.90Q62.50 75.90 60.70 74.20Q58.90 72.50 57.50 70.70L57.50 70.70L50.30 60.70L40.10 75.70Q39.10 75.90 37.50 75.90L37.50 75.90Q33.50 75.90 31.70 74.20Q29.90 72.50 29.90 70.10L29.90 70.10Q29.90 67.10 32.30 64.70L32.30 64.70L41.50 54.30L24.70 48.70Q24.10 46.70 24.10 44.50L24.10 44.50Q24.10 41.70 25.70 39.90Q27.30 38.10 30.10 38.10Z"></path></svg>`,
-		)
-	}, users))
+	// longpoll pub-sub api:
+	http.HandleFunc("/publish", requireBasicAuth(manager.PublishHandler, users))
+	http.HandleFunc("/events", requireBasicAuth(manager.SubscriptionHandler, users))
 
-	log.Println("Serving on:", *serveAddr, "clientJs at:", *staticClientJs, "saving chats to:", *persistFilename)
+	// Serve static files--doing files explicitly instead of http.FileServer so
+	// we can a) be explicit about what is exposed and b) wrap with http basic auth.
+	serveStatic("/js/golongpoll-client.js", *staticDir, "golongpoll-client.js", "text/javascript", users)
+	serveStatic("/js/main.js", *staticDir, "main.js", "text/javascript", users)
+	serveStatic("/css/main.css", *staticDir, "main.css", "text/css", users)
+	serveStatic("/favicon.svg", *staticDir, "favicon.svg", "image/svg+xml", users)
+	serveStatic("/logout-favicon.svg", *staticDir, "logout-favicon.svg", "image/svg+xml", users)
+
+	log.Println("Serving on:", *serveAddr, "staticDir at:", *staticDir, "saving chats to:", *persistFilename)
 	log.Println("Listing Users...")
 	for _, user := range users {
 		log.Println(user.Username)
@@ -86,33 +83,47 @@ func parseAccounts(input string) []User {
 	if len(parts) == 0 {
 		return nil
 	}
-
 	users := make([]User, 0)
-
 	for _, part := range parts {
 		userAndPassword := strings.Split(part, ":")
 		if len(userAndPassword) != 2 || len(userAndPassword[0]) == 0 || len(userAndPassword[1]) == 0 {
 			fmt.Printf("Invalid -accounts value: %q, must be comma separated accounts of the form user:password.", part)
 			return nil
 		}
-
 		users = append(users, User{Username: userAndPassword[0], Password: userAndPassword[1]})
 	}
-
 	return users
+}
+
+func serveStatic(url string, staticDir string, staticFilename string, contentType string, users []User) {
+	http.HandleFunc(url, requireBasicAuth(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", contentType)
+			http.ServeFile(w, r, filepath.Join(staticDir, staticFilename))
+		}, users))
 }
 
 // Modified flavor of function from: https://stackoverflow.com/a/39591234
 // Updated to take a list of acceptable logins instead of a single user/password.
 func requireBasicAuth(handler http.HandlerFunc, accounts []User) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		user, pass, ok := r.BasicAuth()
 		realm := "Please enter your username and password for this site"
 		if !ok || !loginOkay(user, pass, accounts) {
 			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
 			w.WriteHeader(401)
-			w.Write([]byte("Unauthorized.\n"))
+			w.Write([]byte("\n"))
+			// TODO: stylign here--inline instead of fetching file (inline faviconicon data too?)
+			fmt.Fprintf(w, `
+			<html>
+			<head>
+				<title>Unauthorized</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			</head>
+			<body>
+				<h1>Unauthorized.</h1>
+			</body>
+			</html>`)
 			if ok {
 				log.Println("LOGIN FAIL -", r.URL, "- username:", user, "IP:", r.RemoteAddr, "X-FORWARDED-FOR:", r.Header.Get("X-FORWARDED-FOR"))
 			} else {
@@ -120,7 +131,6 @@ func requireBasicAuth(handler http.HandlerFunc, accounts []User) http.HandlerFun
 			}
 			return
 		}
-
 		handler(w, r)
 	}
 }
@@ -131,16 +141,28 @@ func loginOkay(username string, password string, accounts []User) bool {
 			return true
 		}
 	}
-
 	return false
 }
 
 func logoutPage(w http.ResponseWriter, r *http.Request) {
 	// This should force browsers to forget/re-ask for creds.
 	w.WriteHeader(401)
-	generatePageWithContent(w, `Logged out.<a href="/">Home</a>`)
 	user, _, _ := r.BasicAuth()
 	log.Println("Logout -", r.URL, "- username:", user, "IP:", r.RemoteAddr, "X-FORWARDED-FOR:", r.Header.Get("X-FORWARDED-FOR"))
+	fmt.Fprintf(w, `
+	<html>
+	<head>
+		<title>BasicChat - Logged Out</title>
+		<link rel="icon" type="image/svg+xml" href="/logout-favicon.svg">
+		<link rel="stylesheet" href="/css/main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<p>Logged out as %s.</p>
+		<p><a href="/">Home</a></p>
+	</body>
+	</html>
+	`, user)
 }
 
 // TODO: list rooms (categories) by latest activity first, have last msg and time.
@@ -154,12 +176,22 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Failed to get user.\n"))
 		return
 	}
-
 	log.Println("Homepage -", r.URL, "- username:", username, "IP:", r.RemoteAddr, "X-FORWARDED-FOR:", r.Header.Get("X-FORWARDED-FOR"))
-	generatePageWithContent(w, fmt.Sprintf(`
-	<a href="/logout">Logout</a>
-	<br/>
-	Hi, %s!`, username))
+	fmt.Fprintf(w, `
+	<html>
+	<head>
+		<title>BasicChat - Home</title>
+		<link rel="icon" type="image/svg+xml" href="/favicon.svg">
+		<link rel="stylesheet" href="/css/main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<p>Hello, %s.</p>
+		<p><a href="/chat?room=Neat+Stuff">Neat Stuff</a></p>
+		<p><a href="/logout">Logout</a></p>
+	</body>
+	</html>
+	`, username)
 }
 
 // TODO: conversation list
@@ -168,11 +200,6 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 // TODO: enter to auto send
 // TODO: logout link--make sure works as expected
 // TODO: escape data--see micro-chat how it does it.
-// TODO: styling--make own vs others diff color/style/class
-// TODO: UI click to change (increase... and decrease? display N hours or N day ) sinceTime?
-// TODO: and/or have UI actually hide/unhide some?
-// TODO: light vs dark mode -- cookies or url param?
-// TODO: screenshot in readme because it looks pretty
 func chatroomPage(w http.ResponseWriter, r *http.Request) {
 	// Wrapped by requireBasicAuth which will enforce that a real user+password was provided.
 	username, _, ok := r.BasicAuth()
@@ -183,7 +210,7 @@ func chatroomPage(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Failed to get user.\n"))
 		return
 	}
-
+	// TODO: need to make sure room is escaped/sanitized! Use sanitized version below
 	room := r.URL.Query().Get("room")
 	if len(room) < 1 {
 		w.WriteHeader(400)
@@ -191,34 +218,20 @@ func chatroomPage(w http.ResponseWriter, r *http.Request) {
 		log.Println("ChatRoom MISSING ROOM PARAN -", r.URL, "- username:", username, "IP:", r.RemoteAddr, "X-FORWARDED-FOR:", r.Header.Get("X-FORWARDED-FOR"))
 		return
 	}
-
 	log.Println("ChatRoom -", r.URL, "- username:", username, "room:", room, "IP:", r.RemoteAddr, "X-FORWARDED-FOR:", r.Header.Get("X-FORWARDED-FOR"))
-
-	generatePageWithContent(w, fmt.Sprintf(`
-	<a href="/">Home</a>
-	<a href="/logout">Logout</a>
-	<br/>
-	Hi, %s!.  Welcome to chatroom: %s
-	`, username, room))
-}
-
-func generatePageWithContent(w io.Writer, content string) {
 	fmt.Fprintf(w, `
 	<html>
 	<head>
-		<title>Chat</title>
+		<title>BasicChat - %s</title>
 		<link rel="icon" type="image/svg+xml" href="/favicon.svg">
-		<style>
-			body {
-				background-color: white;
-				color: black;
-			    font-family: Arial, Helvetica, sans-serif;
-			}
-		</style>
+		<link rel="stylesheet" href="/css/main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-%s
-	</body>
+		<p>Hi, %s!.  Welcome to chatroom: %s.</p>
+		<p><a href="/">Home</a></p>
+		<p><a href="/logout">Logout</a></p>
+		</body>
 	</html>
-	`, content)
+	`, room, username, room)
 }
